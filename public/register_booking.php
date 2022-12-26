1453ebd78a565ff1f6c5b11c52b7cdb660ebd878
Ronan McCann <ronanemccann@hotmail.com>
2022-12-26 10:13:46 +0000

    Redirection of User To Booking Details Form

--- public/register_booking.php
+++ public/register_booking.php
@@ -6,5 +6,16 @@ require "./booking.php";
 echo $_BOOKING->save ($_POST["date"], $_POST["slot"], $_POST["user"])
   ? "OK" : $_BOOKING->error;
 
+// Retrieve latest booking
+$sql = "SELECT * FROM booking ORDER BY appo_id DESC LIMIT 0,1";
 
+// Query database
+$result = mysqli_query($connection, $sql);
+$row = mysqli_fetch_assoc($result);
+
+// Save details of the latest booking
+$bookingId = $row['booking_id'];
+$userId = $row['user_id'];
+
+header("location:booking_details.php?bookingId=$bookingId&userId=$userId");
 
