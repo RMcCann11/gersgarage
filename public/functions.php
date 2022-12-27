030f7f741427a33366ce2db4f420ae9dd3d2bbce
Ronan McCann <ronanemccann@hotmail.com>
2022-12-27 10:27:57 +0000

    Listing of Bookings Associated With A User Account

--- public/functions.php
+++ public/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+//Used to dynamically pull vehicle details from database
 function showVehiclesBookingDetails(){
 
      // Credentials
@@ -31,6 +32,7 @@ DELIMITER;
 
 }
 
+//Used to dynamically pull product details from database
 function showProductsBookingDetails(){
 
     // Credentials
@@ -57,6 +59,49 @@ $productOptions = <<<DELIMITER
 DELIMITER;
 
     echo $productOptions;
+
+    }
+
+}
+
+//Used to retrieve the details of all bookings by a particular user
+function getBookingsByUser($id){
+
+    // Credentials
+    $dbhost = 'localhost:3307';
+    $dbuser = 'root';
+    $dbpass = 'root';
+    $dbname = 'gersgarage';
+
+    // Create a database connection
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    // create SQL statement
+    $sql = "SELECT * FROM booking_detail WHERE user_id = $id";
+    
+    // Query database
+    $result = mysqli_query($connection, $sql);
+
+    while($row = mysqli_fetch_assoc($result)){
+
+$bookings = <<<DELIMITER
+        
+    <tr>
+        <td>{$row["booking_id"]}</td>
+        <td>{$row["vehicle_id"]}</td>
+        <td>{$row["product_id"]}</td>
+        <td>{$row["license_number"]}</td>
+        <td>{$row["engine_type"]}</td>
+        <td>{$row["cust_name"]}</td>
+        <td>{$row["cust_contact"]}</td>
+        <td>{$row["cust_comment"]}</td>
+        <td>{$row["booking_status_id"]}</td>
+            
+    </tr>
+
+DELIMITER;
+
+        echo $bookings;
 
     }
 
---
+++ public/user_account.php
@@ -0,0 +1,103 @@
+<?php
+
+include '../private/db_connection.php';
+include './functions.php';
+
+$userId = $_GET['userId'];
+
+// create SQL statement
+$sql = "SELECT * FROM user WHERE user_id=$userId";
+
+// Query database
+$result = mysqli_query($connection, $sql);
+$row = mysqli_fetch_assoc($result);
+
+$userName = $row['user_name'];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Ger's Garage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/mystyle.css">
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Make A Booking<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Us<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="review.php">Customer Reviews<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <br><br>
+
+      <div class="container-fluid">
+
+    <div class="jumbotron text-center" id="jtcolour">
+        <h2><u>Welcome <?php echo $userName ?> to your account</u></h2>
+
+    </div>
+
+    <h3 class = "text-center">All Bookings</h3>
+
+    <hr>
+
+    <div class="row">
+
+        <div class="col-sm-6 offset-sm-3">
+
+        <table class="table table-hover">
+
+
+    <thead>
+
+      <tr>
+           <th>Booking Id</th>
+           <th>Vehicle</th>
+           <th>Product</th>
+           <th>License Number</th>
+           <th>Engine Type</th>
+           <th>Booking Name</th>
+           <th>Booking Contact</th>
+           <th>Booking Comment</th>
+           <th>Booking Status</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    <?php getBookingsByUser($userId)?>
+
+  </tbody>
+</table>
+
+    </div>
+
+    </div>
+
+</div>
